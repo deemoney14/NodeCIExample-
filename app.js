aaae64252f39651cf451c0ba6aa8d5e4a89c174d
sammy <sackah08@gmail.com>
2025-02-26 00:05:40 -0800

    second time

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, CI/CD!');
+});
+
+const PORT = process.env.PORT || 3000;
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
